c9140230e1aaf84de093f75bf5cec6bba8a00563
SebaDeca <manuelsebastiandecarvalho@gmail.com>
2023-05-16 22:26:22 -0300

    Elimino prompts y alerts. Comienzo a crear carrito de compras

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-let nombreUsuario = prompt("Ingrese su usuario");
-
-if (nombreUsuario == "") {
-
-    alert("No ingresaste nombre de usuario");
-} else {
-
-    alert("El nombre de usuario es: " + nombreUsuario);
-}
-
-alert("Bienvenido al local de ropa Labasse mayorista");
-
-let monto = parseInt(prompt("La compra al por mayor requiere de un monto mínimo de $50000, por favor confirme cual es el monto pretendido a consumir"));
-
-while (monto < 50000) {
-    alert("el monto " + monto + "$ Es menor al permitido, Le sugerimos visitar la página de venta al público")
-
-    monto = prompt("Ingrese un nuevo monto")
-}
-
-function unaFuncion() {
-    console.log("Bienvenido")
-    return "Los mejores precios al por mayor";
-}
 
 const saludo = unaFuncion();
 console.log(saludo);
